0b3271aba0f16c0e61de5bcaa7d506e58f42b6aa
AFRR <andresrodriguezre@gmail.com>
2026-09-17 10:39:24 -0500

    Se agraga funcion calcular valor del pedido

--- cafeteria.py
+++ cafeteria.py
@@ -1,12 +1,20 @@
+def calcular_total(precio,cantidad):
+    return precio*cantidad
+
 print("================================")
 print("SISTEMA PEDIDOS CAFETRIA-EL MONO")
 print("================================")
 
 producto = input("Ingrese el nombre del producto: ")
 precio = float(input("Ingrese el precio del producto: "))
+cantidad = int(input("Ingrese cantidada: "))
 
+valor_total = calcular_total(precio,cantidad)
 print()
 print("Pedido registrado: ")
 print("Producto: ", producto)
 print("Precio: $ ", precio)
+print("Valor Subtotal: $ ",valor_total)
 print("El producto tiene un descuento del 10%")
+descuento = valor_total*0.9
+print("Valor total: $ ", descuento)
